acf81164cf0429355be079d01da63fcc3d40ce22
rakesh <rakeshreddy@gmail.com>
2025-12-09 15:27:20 +0530

    modified 03-variables.sh with changing variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-Person1=Ramesh
-Person2=Suresh
+Person1=sachin
+Person2=pandey
 echo "$Person1:: welcome to hyd $Person2"
 echo "$Person2:: Thanks! how are you $Person1"
 echo "$Person1:: I am good $Person2. What about you"
