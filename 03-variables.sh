199374507df20a8e4542b06913d43ccdc15792c4
rakesh <rakeshreddy@gmail.com>
2025-12-09 15:23:09 +0530

    added 03-variables.sh

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Person1=Ramesh
+Person2=Suresh
+echo "$Person1:: welcome to hyd $person2"
+echo "$person2:: Thanks! how are you $person1"
+echo "$person1:: I am good $person2. What about you"
+echo "$person2:: Good $person1"
