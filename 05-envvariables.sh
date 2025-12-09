5aeef45cbb99a5257076da19f26163e16f6c1fd9
rakesh <rakeshreddy@gmail.com>
2025-12-09 15:43:38 +0530

    added information for  env variables

--- 05-envvariables.sh
+++ 05-envvariables.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # I have set env variable COURSE by using command COURSE="WELCOME TO SHELL SCRIPTING"
+# This will exist only in current shell.once we exit it will not get the value. To present permanently create export variable in .bashrc hidden folder"
+
 echo "$COURSE" 
