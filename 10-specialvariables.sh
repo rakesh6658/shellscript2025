e7c37d9e06c44c1505bcb60680ed8ca088dbacf8
rakesh <rakeshreddy@gmail.com>
2025-12-10 10:36:54 +0530

    special variables

--- 10-specialvariables.sh
+++ 10-specialvariables.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "to get all the values from command prompt $@ $* "
+echo "to get all the values from command prompt we have both special characters  $@ , $* "
+echo "To get file name $0 "
