be31cef48baceed450dde2b1c70716e227a7faee
rakesh <rakeshreddy@gmail.com>
2025-12-10 10:55:44 +0530

    created conditions shell script

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -5,6 +5,8 @@ number=$1
 if [ $number -lt 100 ]
 then
  echo " $number is less than 100 "
+ elif [ $number -eq 100 ]
+ echo " $number is equal to 100 "
  else
   echo " $number is greater than 100 "
   fi
