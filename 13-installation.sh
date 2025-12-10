8ea6c0e9afb7522d938a564243ca6a1785b5e283
rakesh <rakeshreddy@gmail.com>
2025-12-10 11:48:14 +0530

    created installation shell script

--- 13-installation.sh
+++ 13-installation.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+LOG_DIR="/var/log/shellscript"
+file_name= "$LOG_DIR/installation.log"
+
+mkdir -p $LOG_DIR
+
 number=$(id -u)
 
 if [ $number -ne 0 ]
@@ -18,12 +23,12 @@ echo "installation of $2 success"
 fi
 }
 
-dnf list installed $1
+dnf list installed $1 &>> $file_name
 
 if [ $? -ne 0 ]
 then
 echo " $1 not installed"
-dnf install $1 -y
+dnf install $1 -y &>> $file_name
 validate $? "$1"
 
 else
