afbf20b6fe2b347d2b468422c9adf0f7443d9a97
rakesh <rakeshreddy@gmail.com>
2025-12-10 10:46:41 +0530

    special variables

--- 10-specialvariables.sh
+++ 10-specialvariables.sh
@@ -4,4 +4,6 @@ echo "to get all the values from command prompt we have both special characters 
 echo "To get file name $0 "
 echo "to get the user  $USER"
 echo " to get user home directory $HOME"
-echo " to get the procces id of the shellscript $$ "
+echo " to get the procces id of the current shellscript $$ "
+sleep 10 &
+echo "pid of last background process $!"
