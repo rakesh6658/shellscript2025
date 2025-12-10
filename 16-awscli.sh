e2de0817bfc9cf47dfcc29237a26a33965073307
rakesh <rakeshreddy@gmail.com>
2025-12-10 16:32:11 +0530

    created instance through aws cli

--- 16-awscli.sh
+++ 16-awscli.sh
@@ -2,7 +2,8 @@
 
 AMI_ID=ami-09c813fb71547fc4f
 sg_id=sg-08c5ef089ab83c3ce
-
+HOSTED_ZONE_ID=Z01767341CQ3HG2Z9RB0E
+domain_name=joindevops.store
 
 for instance in $@
 do
@@ -21,6 +22,39 @@ do
   --instance-ids $instance_id \
   --query "Reservations[0].Instances[0].PrivateIpAddress" \
   --output text)
+  if [ $instance -eq frontend ]
+  then
+
+  aws route53 change-resource-record-sets \
+  --hosted-zone-id $HOSTED_ZONE_ID \
+  --change-batch '{
+    "Changes": [{
+      "Action": "CREATE",
+      "ResourceRecordSet": {
+        "Name": "'${instance}.${domain_name}'",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [{ "Value": '"$public_ip"' }]
+      }
+    }]
+  }'
+  else
+  aws route53 change-resource-record-sets \
+  --hosted-zone-id $HOSTED_ZONE_ID \
+  --change-batch '{
+    "Changes": [{
+      "Action": "CREATE",
+      "ResourceRecordSet": {
+        "Name": "'${instance}.${domain_name}'",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [{ "Value": '"$private_ip"' }]
+      }
+    }]
+  }'
+  fi
+
+
   
 
 
