bad66356e200336e612ba92a30b0edae258f2304
rakesh <rakeshreddy@gmail.com>
2025-12-10 10:39:23 +0530

    special variables

--- 10-specialvariables.sh
+++ 10-specialvariables.sh
@@ -3,3 +3,4 @@
 echo "to get all the values from command prompt we have both special characters  $@ , $* "
 echo "To get file name $0 "
 echo "to get the user  $USER"
+echo " to get user home directory $HOME"
