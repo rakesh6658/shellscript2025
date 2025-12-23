0b62e928069e3e16761060068bfb042a1bb49bd5
rakesh <rakeshreddy@gmail.com>
2025-12-23 14:33:56 +0530

    added 17 and 18 script.sh

---
+++ 17-script1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+city=hyderabad
+echo "welcome to $city"
+echo "script id $$"
+sh 18-script2.sh
---
+++ 18-script2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "This is called is another script"
+echo "$city"
+echo "script id $$"
