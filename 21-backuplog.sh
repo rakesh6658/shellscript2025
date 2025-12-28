265e59c6f880ac34c6ae34e117d9dab821bfb234
rakesh <rakeshreddy@gmail.com>
2025-12-28 12:24:58 +0530

    added backuplog

--- 21-backuplog.sh
+++ 21-backuplog.sh
@@ -41,12 +41,6 @@ then
 timestamp=$(date +%F-%H-%M)
 ZIPFILENAME=$($DESTDIR/app-logs-$timestamp.zip)
 echo "find "$SOURCEDIR" -name "*.log" -type f -mtime "+$DAYS"" | zip -@ -j $ZIPFILENAME"
-if [ ! -f $ZIPFILENAME ]
-then
-echo -e "archival is $RED failure $NC"
-else
-echo -e "archival is $GREEN success $NC"
-fi
 
 fi
 done <<< $FILES
