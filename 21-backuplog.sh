7aa35adb0d37a0335eccf758d938c7951c46f3d9
rakesh <rakeshreddy@gmail.com>
2025-12-28 12:46:42 +0530

    added backuplog

--- 21-backuplog.sh
+++ 21-backuplog.sh
@@ -39,8 +39,8 @@ do
 if [ ! -z $file ]
 then
 timestamp=$(date +%F-%H-%M)
-ZIPFILENAME=$($DESTDIR/app-logs-$timestamp.zip)
-echo "find "$SOURCEDIR" -name "*.log" -type f -mtime "+$DAYS"" | zip -@ -j $ZIPFILENAME
+ZIPFILENAME=$DESTDIR/app-logs-$timestamp.zip
+echo find "$SOURCEDIR" -name "*.log" -type f -mtime "+$DAYS" | zip -@ -j $ZIPFILENAME
 if [  -f $ZIPFILENAME ]
 then
 echo -e "archival is $RED failure $NC"
